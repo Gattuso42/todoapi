d7eb5dfb93c7b88daf8791cc572ddb249053eadd
Gattuso42 <ralofguild@gmail.com>
2023-08-04 00:22:41 -0700

    version 1.0.3 version with MySql localhost

---
+++ src/test/resources/queriesTest.sql
@@ -0,0 +1,3 @@
+INSERT INTO TASK VALUES("test1","testing1","pending","2023-08-9");
+INSERT INTO TASK VALUES("test2","testing2","pending","2023-08-9");
+INSERT INTO TASK VALUES("test3","testing3","pending","2023-08-9");
